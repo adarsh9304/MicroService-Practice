f2d1f97b3c4f8ccf992227809b687009362eea11
adarsh <adarsh.patel@bacancy.com>
2024-09-18 17:43:34 +0530

    Adds:Docker into existing microservices

--- gateway/index.js
+++ gateway/index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use(cors())
 
-app.use('/customer',proxy('http://localhost:3001'));
-app.use('/order',proxy('http://localhost:3002'))
+app.use('/customer',proxy('http://custser:3001'));
+app.use('/order',proxy('http://ordser:3002'))
 
 
 app.listen(3000, () => {
